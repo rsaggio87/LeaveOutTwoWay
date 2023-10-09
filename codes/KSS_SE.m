e7b47589f806d4f25ebec7e5ad1ae0298ac7dc74
rsaggio87 <raffaele.saggio@berkeley.edu>
2023-10-09 11:17:51 -0700

    added parfor option
    added parfor option
    improved documentation

--- codes/KSS_SE.m
+++ codes/KSS_SE.m
@@ -6,6 +6,7 @@ function [coeff,SE,SE_naive]  = KSS_SE(y,D,clusterID,year,controls,labels);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 got_labels=0;
+Lchol=[];
 nochol= 0;
 if nargin==6 && ~isempty(labels) 
     got_labels=1;
@@ -54,6 +55,38 @@ res                                = sqrt(adj_REGHDFE)*(y-X*coeff);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 meat                               = zeros(K,K);
 meat_naive                         = zeros(K,K);
+if N_D>100 || Nobs > 10000 %large data, run with parfor
+parfor c=1:N_cluster
+    
+    %% Part 1: Estimate Regression Coefficient After Leaving Cluster c Out
+    sel                            = clusterID ~= c;
+    y_use                          = y(sel);  
+    X_use                          = X(sel,:);
+    xx                             = X_use'*X_use;
+    xy                             = X_use'*y_use;
+    if nochol == 0
+    [coeff_leave_out, flag]		   = pcg(xx,xy,tol,numIterations,Lchol,Lchol'); %coefficient vector after leaving a cluster out.
+    end
+    if nochol == 1
+    [coeff_leave_out, flag]		   = pcg(xx,xy,tol,numIterations); %coefficient vector after leaving a cluster out.
+    end
+    %coeff_leave_out(1:N_D)
+    
+    %% Part 2: Form KSS sigma_i, compute meat of the sandwich
+    sel                            = clusterID==c;
+    y_cluster                      = y(sel);
+    X_cluster                      = X(sel,:);
+    eta_h                          = y_cluster-X_cluster*coeff_leave_out; %leave cluster c out residuals.
+    score_left                     = X_cluster'*(y_cluster-mean(y));
+    score_right                    = X_cluster'*eta_h;
+    meat                           = meat+score_left*score_right'; %X'*sigma_KSS*X for a given cluster
+    
+    res_cluster                    = res(sel);
+    score_left                     = X_cluster'*res_cluster;
+    score_right                    = X_cluster'*res_cluster;
+    meat_naive                     = meat_naive+score_left*score_right'; %X'*sigma_naive*X for a given cluster
+end
+else
 for c=1:N_cluster
     
     %% Part 1: Estimate Regression Coefficient After Leaving Cluster c Out
@@ -83,7 +116,7 @@ for c=1:N_cluster
     score_left                     = X_cluster'*res_cluster;
     score_right                    = X_cluster'*res_cluster;
     meat_naive                     = meat_naive+score_left*score_right'; %X'*sigma_naive*X for a given cluster
-
+end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -115,7 +148,7 @@ if got_labels == 1
     disp(s)
     %s=['Robust "White" Standard Error: ' num2str((SE_naive(q)))];
     %disp(s)
-    s=['KSS Standard Error: ' num2str((SE(q)))];
+    s=['Leave-Out Standard Error: ' num2str((SE(q)))];
     disp(s)
     s=['******************************************'];
     disp(s);
